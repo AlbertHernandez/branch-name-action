8d88fc338eda48a1b2980763fe7c43d36cef2b27
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2020-05-10 12:05:17 +0200

    Add more config files

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ const validateRequiredParameters = require('./src/validations/validate-required-
 Toolkit.run(
   async tools => {
     tools.log.info('Validating required parameters...');
-    validateRequiredParameters(tools, []);
+    validateRequiredParameters(tools, [
+      'branch_pattern',
+    ]);
 
     tools.log.info('Running the action...');
     await runAction(tools);
-
   },
   {
     event: [
       'pull_request.opened',
+      'pull_request.edited',
+      'pull_request.synchronize',
     ],
     secrets: ['GITHUB_TOKEN'],
   },
