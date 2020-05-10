e2ddf7e388a54ce2acd65d5291eadd41a24471bb
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2020-05-10 15:11:28 +0200

    Create first version of the action

---
+++ src/parameters-schema.js
@@ -0,0 +1,15 @@
+module.exports = {
+  branch_pattern: {
+    required: true,
+  },
+  fail_if_invalid_branch_name: {
+    required: false,
+    availableValues: ['true', 'false'],
+  },
+  comment_for_invalid_branch_name: {
+    required: false,
+  },
+  ignore_branch_pattern: {
+    required: false,
+  },
+};
---
+++ src/validations/validate-parameters.js
@@ -0,0 +1,63 @@
+const getParametersWithEmptyValues = (tools, schema) => {
+  return Object.entries(schema).reduce(
+    (acc, [parameter, parameterConfig]) => {
+      if (!parameterConfig.required) {
+        return acc;
+      }
+
+      if (tools.inputs[parameter]) {
+        return acc;
+      }
+
+      acc.push(parameter);
+      return acc;
+    }, [],
+  );
+};
+
+const getParametersWithInvalidValues = (tools, schema) => {
+  return Object.entries(schema).reduce(
+    (acc, [parameter, parameterConfig]) => {
+      if (!parameterConfig.availableValues) {
+        return acc;
+      }
+
+      if (!parameterConfig.required && !tools.inputs[parameter]) {
+        return acc;
+      }
+
+      if (!parameterConfig.availableValues.includes(tools.inputs[parameter])) {
+        acc.push(parameter);
+      }
+
+      return acc;
+    }, [],
+  );
+};
+
+/**
+ * Validate correct parameters of the action
+ * @param {import('actions-toolkit').Toolkit} tools
+ * @param {object} schema
+ */
+module.exports = (tools, schema) => {
+  const parametersWithEmptyValues = getParametersWithEmptyValues(tools, schema);
+
+  if (parametersWithEmptyValues.length) {
+    tools.exit.failure(
+      `You forgot to provide some required values: [${parametersWithEmptyValues.join(
+        ', ',
+      )}]`,
+    );
+  }
+
+  const parametersWithInvalidValues = getParametersWithInvalidValues(tools, schema);
+
+  if (parametersWithInvalidValues.length) {
+    tools.exit.failure(
+      `Some parameters have invalid values: [${parametersWithInvalidValues.join(
+        ', ',
+      )}]`,
+    );
+  }
+};
