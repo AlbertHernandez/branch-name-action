3cbd1501936474ae5a5708fb8d2044d997e384b1
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2020-05-10 12:29:28 +0200

    v7

--- src/run-action.js
+++ src/run-action.js
@@ -3,5 +3,5 @@ const writeComment = require('./github/write-comment');
 module.exports = async tools => {
   const branchPattern = tools.inputs.branch_pattern;
 
-  await writeComment(tools, 'Hello world!');
+  await writeComment(tools, 'Hello world! \n Banana!');
 };
